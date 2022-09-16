cc15e5aea65ed2b3b5464af56882c8594ef565df
Ananthakrishnans007 <ananthakrishnan833@gmail.com>
2022-09-16 17:28:38 +0530

    2nd

--- infoxfinsys/settings.py
+++ infoxfinsys/settings.py
@@ -145,8 +145,9 @@ EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
 
 MEDIA_ROOT = os.path.join(BASE_DIR,'media')
 MEDIA_URL = '/media/'
-STATIC_ROOT = os.path.join(BASE_DIR,'static')
+# STATIC_ROOT = os.path.join(BASE_DIR,'static')
 STATIC_URL = '/static/'
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static'),]
 
 # STATIC_URL = '/static/'
 # STATIC_ROOT = os.path.join(BASE_DIR,'static')
